08e608dd01bab52bb81a63ab044f8cc2551c314c
El Samp <anthony.francisco.aoct@gmail.com>
2023-01-30 13:45:57 -0500

    fixed final print

--- hw1_2.py
+++ hw1_2.py
@@ -1,6 +1,6 @@
-homework_grade = float(input("Enter Homework grade:"))
-midterm_grade = float(input("Enter Midterm grade:"))
-final_grade = float(input("Enter Final grade:"))
+homework_grade = float(input("Enter Homework grade: "))
+midterm_grade = float(input("Enter Midterm grade: "))
+final_grade = float(input("Enter Final grade: "))
 
 homework_weight = .45
 midterm_weight = .25
@@ -9,4 +9,4 @@ final_weight = .3
 calculated_grade = homework_grade * homework_weight + \
     midterm_grade*midterm_weight + final_grade*final_weight
 
-print("Your final grade is: ", calculated_grade)
+print('Your final grade is', round(final_grade,2))
