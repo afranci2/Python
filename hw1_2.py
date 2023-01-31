e6eff516c5656da2995d323a8ef55e725affd9fa
El Samp <anthony.francisco.aoct@gmail.com>
2023-01-31 12:46:00 -0500

    fw

--- hw1_2.py
+++ hw1_2.py
@@ -1,6 +1,6 @@
 homework_grade = float(input("Enter Homework grade: "))
 midterm_grade = float(input("Enter Midterm grade: "))
-final_grade = float(input("Enter Final Eam grade: "))
+final_grade = float(input("Enter Final Exam grade: "))
 
 
 calculated_grade = homework_grade * .45 + midterm_grade*.25 + final_grade*.3
