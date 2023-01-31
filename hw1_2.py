d79bdd17e08a834439f824e39d8706de68ad6efd
El Samp <anthony.francisco.aoct@gmail.com>
2023-01-31 13:04:41 -0500

    wef

--- hw1_2.py
+++ hw1_2.py
@@ -5,4 +5,4 @@ final_grade = float(input("Enter Final Exam grade: "))
 
 calculated_grade = homework_grade * .45 + midterm_grade*.25 + final_grade*.3
 
-print('Your final  is: ', round(calculated_grade,2))
+print('Your final grade is: ', round(calculated_grade,2))
