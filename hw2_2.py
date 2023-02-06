ed89ea3a555c4b42bd55b9aa29863b1aa1864730
El Samp <anthony.francisco.aoct@gmail.com>
2023-02-06 09:53:51 -0500

    fixed hw2

--- hw2_2.py
+++ hw2_2.py
@@ -1,5 +1,7 @@
+accumulator = 0
+
 for i in range(9):
-    accumulator = 0
     score = float(input("Enter score: "))
-    accumulator += score
-print("Your average score for the three weeks is: ", "{:,2f}".format(accumulator))
+    accumulator = accumulator+score
+    print(accumulator)
+print("Your average score for the three weeks is: ", accumulator/9)
