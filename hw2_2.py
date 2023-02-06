5379324f69d9c46a37e20127bc54e2e9563dd56a
El Samp <anthony.francisco.aoct@gmail.com>
2023-02-06 10:26:28 -0500

    fixex comment

--- hw2_2.py
+++ hw2_2.py
@@ -8,19 +8,23 @@
 # the accumulator. Finally, the code calculates and prints the average 
 # score by dividing the accumulator by the number of weeks (9).
 
-# Initialize the variable 'accumulator' to keep track of the total score
+# Initialize the accumulator variable to keep track of the total score
 accumulator = 0
 
-# Iterate over the weeks
+# Iterate over 9 weeks
 for i in range(9):
-    # Ask the user to input the score for a week
-    score = float(input("Enter score for week {}: ".format(i+1)))
+    # Ask the user to input a score
+    score = float(input("Enter score: "))
     
-    # Add the score for the current week to the accumulator
+    # Add the score to the accumulator
     accumulator = accumulator + score
 
-# Calculate the average score for three weeks
-average_score = accumulator / 9
+# Calculate the average score by dividing the accumulator by 9
+average = accumulator / 9
 
-# Print the average score rounded to 2 decimal places
-print("Your average score for three weeks is: ", round(average_score, 2))
+# Round the average to 2 decimal places
+rounded_average = round(average, 2)
+
+# Print the average score
+print("Your average score for three weeks is: ", rounded_average)
+
