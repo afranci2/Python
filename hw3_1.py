8a3903b56e66e443b01ea02696b8acc727602f28
El Samp <anthony.francisco.aoct@gmail.com>
2023-02-13 09:31:14 -0500

    Wef

--- hw3_1.py
+++ hw3_1.py
@@ -1,10 +1,13 @@
 lowest=1000000
 highest=0
+wagon_list = []
 for i in range(1,13):
     wagon_count = int(input("Enter the number of wagons sold in month " + str(i) + " : "))
-    if wagon_count<lowest:
-        lowest=wagon_count
-    if wagon_count>highest:
-        highest=wagon_count
+    wagon_list.append(wagon_count)
+for wagon in wagon_list:
+    if wagon<lowest:
+            lowest=wagon_count
+    if wagon>highest:
+            highest=wagon_count
 difference = highest-lowest
 print("The difference between the most wagons sold and the least wagons sold is ", difference)
