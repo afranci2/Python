b82e7817fd02657628c1ddac74acae16889a7a3c
El Samp <anthony.francisco.aoct@gmail.com>
2023-02-13 09:25:35 -0500

    final

--- hw3_1.py
+++ hw3_1.py
@@ -1,7 +1,7 @@
 lowest=1000000
 highest=0
-for i in range(12):
-    wagon_count = int(input("Enter the number of wagons sold in month ", i, " : "))
+for i in range(1,13):
+    wagon_count = int(input("Enter the number of wagons sold in month " + str(i) + " : "))
     if wagon_count<lowest:
         lowest=wagon_count
     if wagon_count>highest:
