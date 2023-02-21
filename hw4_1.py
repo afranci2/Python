68ad586a5f8ccfe4b799c0774567a081acbc51d6
El Samp <anthony.francisco.aoct@gmail.com>
2023-02-21 14:31:50 -0500

    Erg

--- hw4_1.py
+++ hw4_1.py
@@ -7,7 +7,9 @@ def getStates():
         list_of_abb.append(state_abb)
         state_pop = int(input("Enter state population: "))
         list_of_pop.append(state_pop)
-    return list_of_abb, list_of_pop
+        target = input("Enter a state to find population of: ")
+
+    return list_of_abb, list_of_pop, target
 
 def searchState(list_of_abb, target):
     target_index=-1
@@ -31,8 +33,7 @@ def printResults(list_of_abb, state_index, list_of_pop, higher_state_results):
     print("The states with a higher population than", list_of_abb[state_index], "are:", higher_state_results)
 
 def main():
-    list_of_abb, list_of_pop = getStates()
-    target = input("Enter a state to find population of: ")
+    list_of_abb, list_of_pop, target = getStates()
     state_index = searchState(list_of_abb, target)
     higher_state_results = higherPopStates(list_of_abb, list_of_pop, state_index)
     printResults(list_of_abb, state_index, list_of_pop, higher_state_results)
