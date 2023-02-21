a15806cabcf255b164db93bae60c2c049803fc54
El Samp <anthony.francisco.aoct@gmail.com>
2023-02-21 15:12:11 -0500

    Reg

--- hw4_3.py
+++ hw4_3.py
@@ -2,17 +2,15 @@
 Name: Anthony Francisco
 Date: 2/21/23
 Title: CSC 110 - Homework 4 - Programming in Python
-Description: The sumDigits() function takes a single 
-integer user_number as an input and returns the sum of its
- digits. The function first converts the input user_number 
- to a string called new_number so that each digit can be 
- accessed separately using an index in the following loop.
-In the loop, total_sum is initialized to 0 and iterates over
- each character in new_number. It extracts each digit from
-  the string by using the index i and converts it back to
-  an integer using int(), then adds it to total_sum.
-Finally, the function returns the total_sum which is the 
-sum of all the digits in user_number.
+Description: The sumDigits() function takes a single integer user_number as an input and returns the sum of its digits.
+The function first converts the input user_number to a string called new_number so that each digit can be accessed separately
+using an index in the following loop. In the loop, total_sum is initialized to 0 and iterates over each character in new_number.
+It extracts each digit from the string by using the index i and converts it back to an integer using int(), then adds it to total_sum.
+Finally, the function returns the total_sum which is the sum of all the digits in user_number.
+
+The sumDivisible() function takes a single integer user_input as an input and returns True if the sum of its digits is divisible by
+user_input, and False otherwise. The function uses the sumDigits() function to get the sum of the digits of user_input and then checks
+if the sum is divisible by user_input using the modulo operator %. If it is divisible, the function returns True, and False otherwise.
 
 """
 
@@ -33,7 +31,10 @@ def sumDigits(user_number):
 
 def sumDivisible(user_input):
     result = sumDigits(user_input)
-    if (user_input%result==0):
+    # Check if user_input is divisible by the sum of its digits
+    if (user_input % result == 0):
+        # If it is divisible, return True
         return True
     else:
+        # If it is not divisible, return False
         return False
