c5a4e6ac91866a740e42b168772e13559bcec002
El Samp <anthony.francisco.aoct@gmail.com>
2023-02-21 14:43:07 -0500

    Rg

--- hw4_1.py
+++ hw4_1.py
@@ -27,8 +27,8 @@ def higherPopStates(list_of_abb, list_of_pop, state_index):
     return list_of_higher_states
 
 def printResults(list_of_abb, state_index, list_of_pop, higher_state_results):
-    print("The population of", list_of_abb[state_index], "is", list_of_pop[state_index])
-    print("The states with a higher population than", list_of_abb[state_index], " are:", higher_state_results)
+    print("The population of", list_of_abb[state_index], " is", list_of_pop[state_index])
+    print("The states with a higher population than", list_of_abb[state_index], "are:", higher_state_results)
 
 def main():
     list_of_abb, list_of_pop = getStates()
